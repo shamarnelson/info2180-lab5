aeb9000333847057e01dd1b8eda19308430ff546
shamarnelson <50060085+shamarnelson@users.noreply.github.com>
2020-11-19 23:22:43 -0500

    Added initial starter files

--- world.js
+++ world.js
@@ -1,11 +1,35 @@
-window.onload=function(){
-    var httpRequest=new XMLHttpRequest();
-    var button=document.getElementsByClassName("btn");
+function worldSearch(){
 
-    button[0].addEventListener("click", function(n)){
-
-        n.preventDefault();
-        var url="http://localhost:8888/world.php";
-        
-    }
-}
+    document.getElementById("lookup").addEventListener("click", function(){      
+        var inVal = document.getElementById("country").value;
+        var xhr = new XMLHttpRequest();
+      
+        xhr.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200){
+              
+              document.getElementById("result").innerHTML = xhr.responseText;
+      
+            }
+          }
+          xhr.open("GET", "world.php?country="+inVal);
+          xhr.send();
+        });
+      
+        document.getElementById("lookup-cities").addEventListener("click", function(){      
+        var inVal = document.getElementById("country").value;
+        var xhr = new XMLHttpRequest();
+      
+      
+        xhr.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200){
+              
+              document.getElementById("result").innerHTML = xhr.responseText;
+      
+            }
+          }
+        xhr.open("GET", "world.php?country=" +inVal + "&context=cities");
+        xhr.send();
+        });
+      
+      }
+      window.onload=worldSearch;
